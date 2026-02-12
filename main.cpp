eea4677cb1f970ae9d862614d4d47186d0d16a29
Andrez De La Torre <andrez.code@gmail.com>
2026-02-11 17:59:51 -0800

    Converted my class to an h file

--- main.cpp
+++ main.cpp
@@ -8,54 +8,9 @@
 #include <string>
 #include <vector>
 #include <limits>
+#include "main.h"
 
-class BankAccount {
 
-    public: 
-        BankAccount();
-        BankAccount(std::string accountNumber, std::string accountHolderName, double balance);
-        bool Deposit(double amount) {
-            if (amount > 0) {
-                BankAccount::balance += amount;
-                return true;
-            }
-            return false;  // Invalid amount
-        };
-        bool Withdraw(double amount) {
-            if (amount > 0 && amount <= BankAccount::balance) {
-                BankAccount::balance -= amount;
-                return true;
-            }
-            return false;  // Insufficient funds or invalid amount
-        };
-
-        //Setters
-        void SetBalance(double balance) {
-            BankAccount::balance = balance;
-        };
-        void SetAccountNumber(std::string accountNumber) {
-            BankAccount::accountNumber = accountNumber;
-        };
-        void SetAccountHolderName(std::string accountHolderName) {
-            BankAccount::accountHolderName = accountHolderName;
-        };
-
-        //Getters
-        double GetBalance() const {
-            return BankAccount::balance;
-        };
-        std::string GetAccountNumber() const {
-            return BankAccount::accountNumber;
-        };
-        std::string GetAccountHolderName() const {
-            return BankAccount::accountHolderName;
-        };
-
-    private:
-        std::string accountNumber;
-        std::string accountHolderName;
-        double balance;
-};
 
 //This is the default constructor
 BankAccount::BankAccount() {
